5dd7ae4d68180b89162fa25cc0cb394a32e2ae06
juanlacy <juan.lacy@huelemu.com.ar>
2025-09-14 23:33:57 -0300

    finindex

--- index.php
+++ index.php
@@ -21,142 +21,173 @@
     <link href="libs/css/google-opensans.css" rel="stylesheet">
     <script src="libs/js/app/jquery.min.js"></script>
 
-    <title>Transformación Digital - SkyTel</title>
+    <title>Procesos SkyTel</title>
+
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            margin: 0;
+            height: 100vh;
+        }
+        header {
+            width: 100%;
+            padding: 10px;
+            background-color: #f4f4f4;
+            border-bottom: 1px solid #ccc;
+            box-sizing: border-box;
+        }
+        #content {
+            display: flex;
+            flex: 1;
+        }
+        #indice {
+            width: 300px;
+            /* border-right: 1px solid #ccc; */
+            padding: 5px;
+            box-sizing: border-box;
+            overflow-y: auto;
+        }
+        #iframe-container {
+            width: 100%;
+            padding: 5px;
+            box-sizing: border-box;
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+        .placeholder {
+            font-size: 24px;
+            text-align: center;
+            margin-top: 20%;
+        }   
+        #iframe-container .placeholder {
+            display: none; /* Oculta el placeholder inicialmente */
+        }
+        #iframe-container #miIframe:loaded + .placeholder {
+            display: block; /* Muestra el placeholder si el iframe no carga */
+        }
+      </style>
 </head>
 <body>
+
     <div id="content">
         <div id="indice">
-            <a href="#" class="biz-ex-navigate biz-ex-logo-navigate" onclick="location.reload()">
-                <i class="biz-ex-logo-img"></i>
-            </a>
+        <a href="#" class="biz-ex-navigate biz-ex-logo-navigate" onclick="location.reload()">
+            <i class="biz-ex-logo-img"></i>
+        </a>
+       
+       <h1 class="biz-ex-title-process-jml">Herramientas:</h1>
+        <ul class="nav-bar">
+        <?php
+            $directorio = "herramientas";
+            $subdirectorios = [];
 
-            <h1 class="biz-ex-title-process-jml">Herramientas:</h1>
-            <ul class="nav-bar">
-            <?php
-                $directorio = "herramientas";
-                $subdirectorios = [];
-
-                // Abrir el directorio y recoger todos los subdirectorios en un array
-                if (is_dir($directorio)) {
-                    if ($dh = opendir($directorio)) {
-                        while (($subdirectorio = readdir($dh)) !== false) {
-                            if ($subdirectorio != "." && $subdirectorio != "..") {
-                                $subdirectorios[] = $subdirectorio;
-                            }
+            // Abrir el directorio y recoger todos los subdirectorios en un array
+            if (is_dir($directorio)) {
+                if ($dh = opendir($directorio)) {
+                    while (($subdirectorio = readdir($dh)) !== false) {
+                        if ($subdirectorio != "." && $subdirectorio != "..") {
+                            $subdirectorios[] = $subdirectorio;
                         }
-                        closedir($dh);
                     }
+                    closedir($dh);
                 }
+            }
 
-                // Ordenar alfabéticamente el array de subdirectorios
-                sort($subdirectorios);
+            // Ordenar alfabéticamente el array de subdirectorios
+            sort($subdirectorios);
 
-                // Generar la lista ordenada
-                foreach ($subdirectorios as $subdirectorio) {
-                    // Construimos la ruta completa al archivo index.html
-                    $ruta_index = "$directorio/$subdirectorio/index.html";
-                    echo "<li><a href='$ruta_index' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>$subdirectorio</div></a></li>";
-                }
-            ?>
-            </ul>
+            // Generar la lista ordenada
+            foreach ($subdirectorios as $subdirectorio) {
+                // Construimos la ruta completa al archivo index.html
+                $ruta_index = "$directorio/$subdirectorio/index.html";
+                echo "<li><a href='$ruta_index' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>$subdirectorio</div></a></li>";
+            }
+        ?>
+       
+       
+        <h1 class="biz-ex-title-process-jml">Procesos:</h1>
+        <ul class="nav-bar">
+        <?php
+            $directorio = "procesos";
+            $subdirectorios = [];
 
-            <h1 class="biz-ex-title-process-jml">Procesos:</h1>
-            <ul class="nav-bar">
-            <?php
-                $directorio = "procesos";
-                $subdirectorios = [];
-
-                // Abrir el directorio y recoger todos los subdirectorios en un array
-                if (is_dir($directorio)) {
-                    if ($dh = opendir($directorio)) {
-                        while (($subdirectorio = readdir($dh)) !== false) {
-                            if ($subdirectorio != "." && $subdirectorio != "..") {
-                                $subdirectorios[] = $subdirectorio;
-                            }
+            // Abrir el directorio y recoger todos los subdirectorios en un array
+            if (is_dir($directorio)) {
+                if ($dh = opendir($directorio)) {
+                    while (($subdirectorio = readdir($dh)) !== false) {
+                        if ($subdirectorio != "." && $subdirectorio != "..") {
+                            $subdirectorios[] = $subdirectorio;
                         }
-                        closedir($dh);
                     }
+                    closedir($dh);
                 }
+            }
 
-                // Ordenar alfabéticamente el array de subdirectorios
-                sort($subdirectorios);
+            // Ordenar alfabéticamente el array de subdirectorios
+            sort($subdirectorios);
 
-                // Generar la lista ordenada
-                foreach ($subdirectorios as $subdirectorio) {
-                    // Construimos la ruta completa al archivo index.html
-                    $ruta_index = "$directorio/$subdirectorio/index.html";
-                    echo "<li><a href='$ruta_index' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>$subdirectorio</div></a></li>";
-                }
-            ?>
-            </ul>
+            // Generar la lista ordenada
+            foreach ($subdirectorios as $subdirectorio) {
+                // Construimos la ruta completa al archivo index.html
+                $ruta_index = "$directorio/$subdirectorio/index.html";
+                echo "<li><a href='$ruta_index' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>$subdirectorio</div></a></li>";
+            }
+        ?>
 
-            <h1 class="biz-ex-title-process-jml">Video - Cursos:</h1>
-            <ul class="nav-bar">
-            <?php
-                $directorio = "capacitaciones";
-                $subdirectorios = [];
+        <h1 class="biz-ex-title-process-jml">Videos:</h1>
+        <ul class="nav-bar">
+        <?php
+            $directorio = "capacitaciones";
+            $subdirectorios = [];
 
-                // Abrir el directorio y recoger todos los subdirectorios en un array
-                if (is_dir($directorio)) {
-                    if ($dh = opendir($directorio)) {
-                        while (($subdirectorio = readdir($dh)) !== false) {
-                            if ($subdirectorio != "." && $subdirectorio != "..") {
-                                $subdirectorios[] = $subdirectorio;
-                            }
+            // Abrir el directorio y recoger todos los subdirectorios en un array
+            if (is_dir($directorio)) {
+                if ($dh = opendir($directorio)) {
+                    while (($subdirectorio = readdir($dh)) !== false) {
+                        if ($subdirectorio != "." && $subdirectorio != "..") {
+                            $subdirectorios[] = $subdirectorio;
                         }
-                        closedir($dh);
                     }
+                    closedir($dh);
                 }
+            }
 
-                // Ordenar alfabéticamente el array de subdirectorios
-                sort($subdirectorios);
+            // Ordenar alfabéticamente el array de subdirectorios
+            sort($subdirectorios);
 
-                // Generar la lista ordenada
-                foreach ($subdirectorios as $subdirectorio) {
-                    // Construimos la ruta completa al archivo index.html
-                    $ruta_index = "$directorio/$subdirectorio/index.html";
-                    echo "<li><a href='$ruta_index' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>$subdirectorio</div></a></li>";
-                }
-            ?>
-            </ul>
+            // Generar la lista ordenada
+            foreach ($subdirectorios as $subdirectorio) {
+                // Construimos la ruta completa al archivo index.html
+                $ruta_index = "$directorio/$subdirectorio/index.html";
+                echo "<li><a href='$ruta_index' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>$subdirectorio</div></a></li>";
+            }
+        ?>
+  
+        <h1 class="biz-ex-title-process-jml">Recursos Compartidos SkyTel:</h1>
+        <li><a href='https://sites.google.com/skytel.tech/gws/multimedia?authuser=0' target='_blank' data-new-tab='true' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Presentaciones</div></a></li>
+        <li><a href='https://docs.google.com/spreadsheets/d/1Q5wFyJzWCCa-pXd2-4Ij6th8qyEGAr9Crnam8HvCjYQ/edit?gid=0#gid=0' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Alineacion...</div></a></li>
+        <li><a href='https://docs.google.com/spreadsheets/d/1sfQt0OiVdjXrblLBhWgSL0CmLk_MzaVKON6xh6nGbNk/edit?gid=1681975588#gid=1681975588' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Mapa de Procesos</div></a></li>
+        <li><a href='https://skytel.atlassian.net/servicedesk/customer/portal/24/article/1067614280' target='_blank' data-new-tab='true' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Contact Center</div></a></li>
+        <li><a href='https://sistemagestion.skytel.tech' target='_blank' data-new-tab='true' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Sistema de Gestion</div></a></li>
 
-            <h1 class="biz-ex-title-process-jml">Recursos Compartidos SkyTel:</h1>
-            <ul class="nav-bar">
-                <li><a href='https://sites.google.com/skytel.tech/gws/multimedia?authuser=0' target='_blank' data-new-tab='true' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Presentaciones</div></a></li>
-                <li><a href='https://docs.google.com/spreadsheets/d/1Q5wFyJzWCCa-pXd2-4Ij6th8qyEGAr9Crnam8HvCjYQ/edit?gid=0#gid=0' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Alineacion...</div></a></li>
-                <li><a href='https://docs.google.com/spreadsheets/d/1sfQt0OiVdjXrblLBhWgSL0CmLk_MzaVKON6xh6nGbNk/edit?gid=1681975588#gid=1681975588' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Mapa de Procesos</div></a></li>
-                <li><a href='https://skytel.atlassian.net/servicedesk/customer/portal/24/article/1067614280' target='_blank' data-new-tab='true' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Contact Center</div></a></li>
-                <li><a href='https://sistemagestion.skytel.tech' target='_blank' data-new-tab='true' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Sistema de Gestion</div></a></li>
-            </ul>
 
-            <!-- Sección de usuario al final del menú -->
-            <div class="user-section">
-                <div class="user-info">
-                    <?php if (isset($_SESSION['user']['picture']) && !empty($_SESSION['user']['picture'])): ?>
-                        <img src="<?= htmlspecialchars($_SESSION['user']['picture']) ?>" alt="Avatar" class="user-avatar">
-                    <?php else: ?>
-                        <div class="user-avatar user-avatar-fallback">
-                            <?= strtoupper(substr($_SESSION['user']['name'] ?? 'U', 0, 1)) ?>
-                        </div>
-                    <?php endif; ?>
-                    <div class="user-details">
-                        <div class="user-name"><?= htmlspecialchars($_SESSION['user']['name'] ?? 'Usuario') ?></div>
-                        <div class="user-email"><?= htmlspecialchars($_SESSION['user']['email'] ?? '') ?></div>
-                    </div>
-                </div>
-                <a href="logout.php" class="logout-btn" onclick="return confirm('¿Estás seguro de que quieres cerrar sesión?')">
-                    Cerrar Sesión
-                </a>
-            </div>
+        <!-- <h1 class="biz-ex-title-process-jml">Ayuda Memoria:</h1> -->
+        <!-- <li><a href='https://docs.google.com/presentation/d/11dTQCMk80yQFCJAR7RSSSNqE-TYukXfZmNjOTJy3g2o/edit?usp=sharing' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Transformacion</div></a></li> -->
+        <!-- <li><a href='https://docs.google.com/presentation/d/1jQZMtX5CJsDozaEDyojRRZdXvOrPKN9v/edit?usp=sharing&ouid=101540677606614220156&rtpof=true&sd=true' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Presentacion Procesos</div></a></li> -->
+
+        </ul> 
         </div>
 
         <div style="width:20px; border-right: 1px solid #ccc;">
             <img id="menu-contract" style="float:right; margin-left:2px; margin-right:2px;" src="libs/img/bzg-panel-contract.svg" class="biz-ex-svg-icon biz-ex-svg-toggle biz-ex-menu-visible-toggle biz-ex-menu-toggle-hide" alt="">
             <img id="menu-expand" style="float:right; margin-left:2px; margin-right:2px;" src="libs/img/bzg-panel-expand.svg" class="biz-ex-svg-icon biz-ex-svg-toggle biz-ex-menu-visible-toggle biz-ex-menu-toggle-show" alt="">
         </div>
-
         <div id="iframe-container">
-            <div class="placeholder" id="placeholder">Portal de Transformación Digital SkyTel</div>
+            <div class="placeholder" id="placeholder">JML (Este texto se ocultará una vez que el iframe cargue)</div>
             <iframe id="miIframe" title="Portal de Gestion" width="1140" height="541.25" src="https://pgoyn.skytel.com.ar/" frameborder="0" allowFullScreen="true"></iframe>
         </div>
     </div>
@@ -172,11 +203,6 @@
             enlace.addEventListener('click', (event) => {
                 if (enlace.getAttribute('data-new-tab') === 'true') {
                     // Si el enlace tiene el atributo data-new-tab, no hacemos nada para permitir la apertura en una nueva pestaña
-                    return;
-                }
-
-                // Ignorar clics en el botón de logout
-                if (enlace.classList.contains('logout-btn')) {
                     return;
                 }
 
@@ -199,27 +225,8 @@
             $('#indice').show();
             $('#menu-contract').removeClass("biz-ex-menu-toggle-show").addClass("biz-ex-menu-toggle-hide");
         });
-
-        // Auto-logout por inactividad
-        let inactivityTime = function () {
-            let time;
-            window.onload = resetTimer;
-            document.onmousemove = resetTimer;
-            document.onkeypress = resetTimer;
-
-            function logout() {
-                alert('Sesión expirada por inactividad');
-                window.location.href = 'logout.php';
-            }
-
-            function resetTimer() {
-                clearTimeout(time);
-                time = setTimeout(logout, <?= SESSION_LIFETIME * 1000 ?>);
-            }
-        };
-
-        inactivityTime();
     });
-    </script>
+</script>
+
 </body>
 </html>
