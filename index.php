333be3bb9534a709dfa1e049602563c3edb5b08a
juanlacy <juan.lacy@huelemu.com.ar>
2025-09-14 22:53:36 -0300

    Nuevo Menu

--- index.php
+++ index.php
@@ -20,6 +20,34 @@
         <a href="#" class="biz-ex-navigate biz-ex-logo-navigate" onclick="location.reload()">
             <i class="biz-ex-logo-img"></i>
         </a>
+        <h1 class="biz-ex-title-process-jml">Herramientas:</h1>
+        <ul class="nav-bar">
+        <?php
+            $directorio = "herramientas";
+            $subdirectorios = [];
+
+            // Abrir el directorio y recoger todos los subdirectorios en un array
+            if (is_dir($directorio)) {
+                if ($dh = opendir($directorio)) {
+                    while (($subdirectorio = readdir($dh)) !== false) {
+                        if ($subdirectorio != "." && $subdirectorio != "..") {
+                            $subdirectorios[] = $subdirectorio;
+                        }
+                    }
+                    closedir($dh);
+                }
+            }
+
+            // Ordenar alfabéticamente el array de subdirectorios
+            sort($subdirectorios);
+
+            // Generar la lista ordenada
+            foreach ($subdirectorios as $subdirectorio) {
+                // Construimos la ruta completa al archivo index.html
+                $ruta_index = "$directorio/$subdirectorio/index.html";
+                echo "<li><a href='$ruta_index' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>$subdirectorio</div></a></li>";
+            }
+        ?>
         <h1 class="biz-ex-title-process-jml">Procesos:</h1>
         <ul class="nav-bar">
         <?php
@@ -49,9 +77,35 @@
             }
         ?>
   
-        <!-- <h1 class="biz-ex-title-process-jml">Oficina de Proyectos:</h1> -->
-        <!-- <li><a href='https://app.powerbi.com/view?r=eyJrIjoiMjcxYTNhYjktZTQ5OC00Y2MyLWJkOTgtNDRhNTcyZTg4ZTE1IiwidCI6IjFmNTNjYTlkLTg1YzItNDcwYS1iYTFiLTY5YzExNTcwZTI0NyIsImMiOjR9' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Tablero PMO</div></a></li> -->
+        <h1 class="biz-ex-title-process-jml">Video - Cursos:</h1>
+        <ul class="nav-bar">
+        <?php
+            $directorio = "capacitaciones";
+            $subdirectorios = [];
 
+            // Abrir el directorio y recoger todos los subdirectorios en un array
+            if (is_dir($directorio)) {
+                if ($dh = opendir($directorio)) {
+                    while (($subdirectorio = readdir($dh)) !== false) {
+                        if ($subdirectorio != "." && $subdirectorio != "..") {
+                            $subdirectorios[] = $subdirectorio;
+                        }
+                    }
+                    closedir($dh);
+                }
+            }
+
+            // Ordenar alfabéticamente el array de subdirectorios
+            sort($subdirectorios);
+
+            // Generar la lista ordenada
+            foreach ($subdirectorios as $subdirectorio) {
+                // Construimos la ruta completa al archivo index.html
+                $ruta_index = "$directorio/$subdirectorio/index.html";
+                echo "<li><a href='$ruta_index' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>$subdirectorio</div></a></li>";
+            }
+        ?>
+       
         <h1 class="biz-ex-title-process-jml">Recursos Compartidos SkyTel:</h1>
         <li><a href='https://sites.google.com/skytel.tech/gws/multimedia?authuser=0' target='_blank' data-new-tab='true' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Presentaciones</div></a></li>
         <li><a href='https://docs.google.com/spreadsheets/d/1Q5wFyJzWCCa-pXd2-4Ij6th8qyEGAr9Crnam8HvCjYQ/edit?gid=0#gid=0' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Alineacion...</div></a></li>
@@ -64,7 +118,32 @@
         <!-- <li><a href='https://docs.google.com/presentation/d/11dTQCMk80yQFCJAR7RSSSNqE-TYukXfZmNjOTJy3g2o/edit?usp=sharing' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Transformacion</div></a></li> -->
         <!-- <li><a href='https://docs.google.com/presentation/d/1jQZMtX5CJsDozaEDyojRRZdXvOrPKN9v/edit?usp=sharing&ouid=101540677606614220156&rtpof=true&sd=true' class='biz-ex-navigate'><div class='truncate-text biz-ex-menu'>Presentacion Procesos</div></a></li> -->
 
-        </ul> 
+                </ul>
+            </div>
+            
+            <!-- Sección de usuario al final del menú -->
+            <div class="user-section">
+                <div class="user-info">
+                    <?php if (isset($_SESSION['user']['picture']) && !empty($_SESSION['user']['picture'])): ?>
+                        <img src="<?= htmlspecialchars($_SESSION['user']['picture']) ?>" alt="Avatar" class="user-avatar" style="background: none;">
+                    <?php else: ?>
+                        <div class="user-avatar">
+                            <?= strtoupper(substr($_SESSION['user']['name'] ?? 'U', 0, 1)) ?>
+                        </div>
+                    <?php endif; ?>
+                    <div class="user-details">
+                        <div class="user-name"><?= htmlspecialchars($_SESSION['user']['name'] ?? 'Usuario') ?></div>
+                        <div class="user-email"><?= htmlspecialchars($_SESSION['user']['email'] ?? '') ?></div>
+                    </div>
+                </div>
+                <a href="logout.php" class="logout-btn" onclick="return confirm('¿Estás seguro de que quieres cerrar sesión?')">
+                    <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M17,8L15.59,6.59L13.17,9.01L13.17,2L11.17,2L11.17,9.01L8.75,6.59L7.34,8L12.17,12.83L17,8Z"/>
+                        <path d="M19,15V18C19,19.1 18.1,20 17,20H7C5.9,20 5,19.1 5,18V15H3V18C3,20.21 4.79,22 7,22H17C19.21,22 21,20.21 21,18V15H19Z"/>
+                    </svg>
+                    Cerrar Sesión
+                </a>
+            </div>
         </div>
 
         <div style="width:20px; border-right: 1px solid #ccc;">
@@ -88,6 +167,11 @@
             enlace.addEventListener('click', (event) => {
                 if (enlace.getAttribute('data-new-tab') === 'true') {
                     // Si el enlace tiene el atributo data-new-tab, no hacemos nada para permitir la apertura en una nueva pestaña
+                    return;
+                }
+
+                // Ignorar clics en el botón de logout
+                if (enlace.classList.contains('logout-btn')) {
                     return;
                 }
 
